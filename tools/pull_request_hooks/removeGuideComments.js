ca60a53c6669af02e8a0f4f981c37f24e9eaf7fa
harry <me@harryob.live>
2025-04-27 10:44:11 +0100

    Update removeGuideComments.js

--- tools/pull_request_hooks/removeGuideComments.js
+++ tools/pull_request_hooks/removeGuideComments.js
@@ -16,14 +16,17 @@ function escapeRegex(string) {
 }
 
 export async function removeGuideComments({ github, context }) {
-  const originalBody = (await github.rest.pulls.get({
+  const pr = await github.rest.pulls.get({
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: context.payload.pull_request.number,
-  })).body;
+  })
 
+  const originalBody = pr.body;
+	
   if (!originalBody) {
 	console.log("PR body is empty, skipping...");
+	console.log(pr)
 	return;
   }
 
