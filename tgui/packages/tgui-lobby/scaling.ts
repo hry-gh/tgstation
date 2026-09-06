5f754e2d8793914ffdb3a40fb404caf693118828
harryob <55142896+harryob@users.noreply.github.com>
2026-09-06 18:05:13 +0100

    fudge it a little bigger...

--- tgui/packages/tgui-lobby/scaling.ts
+++ tgui/packages/tgui-lobby/scaling.ts
@@ -1,6 +1,8 @@
+const SCALE_FUDGE = 1.2;
+
 export function updateScaling() {
   document.documentElement.style.setProperty(
     '--lobby-scale',
-    `${window.devicePixelRatio}`,
+    `${window.devicePixelRatio * SCALE_FUDGE}`,
   );
 }
